c6e0eabd39e045f835b95b8ba6db2763f7909984
pokotsun <gorigolila@gmail.com>
2018-09-24 01:06:31 +0900

    Revert "getEventの席の総計がおかしい問題を修正"
    This reverts commit f9fba1cf7b5ba83d82704928418e5517127312da.

--- webapp/go/src/torb/app.go
+++ webapp/go/src/torb/app.go
@@ -131,6 +131,7 @@ func getEvent(eventID, loginUserID int64) (*Event, error) {
         return nil, err
     }
     defer rows.Close()
+    event.Total = 0
 
     for rows.Next() {
         var sheet Sheet
