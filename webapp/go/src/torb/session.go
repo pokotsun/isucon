80d798df1273e6844f72cdbdaa1e74cbfaa28a44
pokotsun <gorigolila@gmail.com>
2018-09-23 17:23:39 +0900

    sessionを切り分けた

---
+++ webapp/go/src/torb/session.go
@@ -0,0 +1,88 @@
+package main
+
+
+import(
+	"github.com/gorilla/sessions"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo-contrib/session"
+)
+
+func sessUserID(c echo.Context) int64 {
+	sess, _ := session.Get("session", c)
+	var userID int64
+	if x, ok := sess.Values["user_id"]; ok {
+		userID, _ = x.(int64)
+	}
+	return userID
+}
+
+func sessSetUserID(c echo.Context, id int64) {
+	sess, _ := session.Get("session", c)
+	sess.Options = &sessions.Options{
+		Path:     "/",
+		MaxAge:   3600,
+		HttpOnly: true,
+	}
+	sess.Values["user_id"] = id
+	sess.Save(c.Request(), c.Response())
+}
+
+func sessDeleteUserID(c echo.Context) {
+	sess, _ := session.Get("session", c)
+	sess.Options = &sessions.Options{
+		Path:     "/",
+		MaxAge:   3600,
+		HttpOnly: true,
+	}
+	delete(sess.Values, "user_id")
+	sess.Save(c.Request(), c.Response())
+}
+
+func sessAdministratorID(c echo.Context) int64 {
+	sess, _ := session.Get("session", c)
+	var administratorID int64
+	if x, ok := sess.Values["administrator_id"]; ok {
+		administratorID, _ = x.(int64)
+	}
+	return administratorID
+}
+
+func sessSetAdministratorID(c echo.Context, id int64) {
+	sess, _ := session.Get("session", c)
+	sess.Options = &sessions.Options{
+		Path:     "/",
+		MaxAge:   3600,
+		HttpOnly: true,
+	}
+	sess.Values["administrator_id"] = id
+	sess.Save(c.Request(), c.Response())
+}
+
+func sessDeleteAdministratorID(c echo.Context) {
+	sess, _ := session.Get("session", c)
+	sess.Options = &sessions.Options{
+		Path:     "/",
+		MaxAge:   3600,
+		HttpOnly: true,
+	}
+	delete(sess.Values, "administrator_id")
+	sess.Save(c.Request(), c.Response())
+}
+
+func loginRequired(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		if _, err := getLoginUser(c); err != nil {
+			return resError(c, "login_required", 401)
+		}
+		return next(c)
+	}
+}
+
+func adminLoginRequired(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		if _, err := getLoginAdministrator(c); err != nil {
+			return resError(c, "admin_login_required", 401)
+		}
+		return next(c)
+	}
+}
