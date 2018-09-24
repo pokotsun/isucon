5e1a768b4e7e192c549039805f479f67e3b9dab8
pokotsun <gorigolila@gmail.com>
2018-09-24 17:03:48 +0900

    Revert "userIDだけにINDEXを貼ってみた"
    This reverts commit fc9da5537efc83aabb1bb7e8d3bb15fd7e369aef.

--- db/schema.sql
+++ db/schema.sql
@@ -29,8 +29,7 @@ CREATE TABLE IF NOT EXISTS reservations (
     user_id     INTEGER UNSIGNED NOT NULL,
     reserved_at DATETIME(6)      NOT NULL,
     canceled_at DATETIME(6)      DEFAULT NULL,
-    KEY event_id_and_sheet_id_idx (event_id, sheet_id),
-    INDEX idx_user(user_id)
+    KEY event_id_and_sheet_id_idx (event_id, sheet_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS administrators (
