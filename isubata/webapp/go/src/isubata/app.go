c79e35436e3e9459833797ead13fa99f1be7ab72
pokotsun <gorigolila@gmail.com>
2019-05-19 16:27:25 +0900

    messageの部分を消した

--- isubata/webapp/go/src/isubata/app.go
+++ isubata/webapp/go/src/isubata/app.go
@@ -337,28 +337,28 @@ func queryChannels() ([]int64, error) {
 }
 
 func queryHaveRead(userID, chID int64) (int64, error) {
-	//type HaveRead struct {
-	//	UserID    int64     `db:"user_id"`
-	//	ChannelID int64     `db:"channel_id"`
-	//	MessageID int64     `db:"message_id"`
-	//	UpdatedAt time.Time `db:"updated_at"`
-	//	CreatedAt time.Time `db:"created_at"`
-	//}
-	//h := HaveRead{}
+	type HaveRead struct {
+		UserID    int64     `db:"user_id"`
+		ChannelID int64     `db:"channel_id"`
+		MessageID int64     `db:"message_id"`
+		UpdatedAt time.Time `db:"updated_at"`
+		CreatedAt time.Time `db:"created_at"`
+	}
+	h := HaveRead{}
 
-	//err := db.Get(&h, "SELECT * FROM haveread WHERE user_id = ? AND channel_id = ?",
-	//	userID, chID)
-	var messageID int64
-	err := db.Get(&messageID, "SELECT message_id FROM haveread WHERE user_id = ? AND channel_id = ?",
+	err := db.Get(&h, "SELECT * FROM haveread WHERE user_id = ? AND channel_id = ?",
 		userID, chID)
+	//var messageID int64
+	//err := db.Get(&messageID, "SELECT message_id FROM haveread WHERE user_id = ? AND channel_id = ?",
+	//	userID, chID)
 
 	if err == sql.ErrNoRows {
 		return 0, nil
 	} else if err != nil {
 		return 0, err
 	}
-	//return h.MessageID, nil
-	return messageID, nil
+	return h.MessageID, nil
+	//return messageID, nil
 }
 
 //TODO なんか遅い
