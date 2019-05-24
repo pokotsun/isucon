9c242e1943dfccc2e711ae0d42583a890a2f84fe
pokotsun <gorigolila@gmail.com>
2019-05-24 12:54:46 +0900

    comment out time.sleep

--- isubata/webapp/go/src/isubata/app.go
+++ isubata/webapp/go/src/isubata/app.go
@@ -346,7 +346,7 @@ func fetchUnread(c echo.Context) error {
 		return c.NoContent(http.StatusForbidden)
 	}
 
-	time.Sleep(time.Second)
+	//time.Sleep(time.Second)
 
 	channels, err := queryChannels() // TODO
 	if err != nil {
