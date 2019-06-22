2926159f401449bfa4fa9a01254efb711b2af0e9
pokotsun <gorigolila@gmail.com>
2019-06-22 11:46:42 +0900

    modify deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-git reset --hard HEAD && git pull origin app/divide-app-code
+git reset --hard HEAD && git pull origin dev
 #sudo ./isubata/db/init.sh
 #zcat ~/isubata/bench/isucon7q-initial-dataset.sql.gz | sudo mysql isubata
-cd isubata/webapp/go && make
+cd isuda/webapp/go && make
 cd ../../..
 ## logからデータを削除
 sudo sh -c 'echo "" > /var/log/nginx/access.log'
@@ -11,6 +11,7 @@ sudo sh -c 'echo "" > /var/log/nginx/access.log'
 #sudo sh -c 'echo "" > /var/log/mariadb/general.sql'
 sudo systemctl restart nginx.service
 sudo systemctl restart mysql.service
-sudo systemctl restart isubata.golang
-cd isubata/bench && ./bin/bench -remotes=127.0.0.1 -output result.json
-jq . < result.json
+sudo systemctl restart isuda.golang
+sudo systemctl restart isutar.golang
+#cd isubata/bench && ./bin/bench -remotes=127.0.0.1 -output result.json
+#jq . < result.json
