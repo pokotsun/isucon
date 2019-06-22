0420f82827793c94e354ca1eaa2af7201f15b615
pokotsun <gorigolila@gmail.com>
2019-06-22 14:39:21 +0900

    replace regex to replace

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,11 @@ git reset --hard HEAD && git pull origin dev
 #sudo ./isubata/db/init.sh
 #zcat ~/isubata/bench/isucon7q-initial-dataset.sql.gz | sudo mysql isubata
 cd webapp/go && make
-cd ../../..
+cd ../..
 ## logからデータを削除
 sudo sh -c 'echo "" > /var/log/nginx/access.log'
+sudo sh -c 'echo "" > /var/log/mysql/slow.log'
+sudo cp conf/my.cnf /etc/mysql/my.cnf
 #sudo sh -c 'echo "" > /var/log/mariadb/slow.sql'
 #sudo sh -c 'echo "" > /var/log/mariadb/general.sql'
 sudo systemctl restart isuda.go
--- webapp/go/isuda.go
+++ webapp/go/isuda.go
@@ -316,31 +316,34 @@ func htmlify(w http.ResponseWriter, r *http.Request, content string) string {
 	`)
 	panicIf(err)
 	keywords := make([]string, 0, 500)
+	kw2sha := make(map[string]string)
 	for rows.Next() {
 		var keyword string
 		err := rows.Scan(&keyword)
 		panicIf(err)
-		keywords = append(keywords, regexp.QuoteMeta(keyword))
+
+		keyword = regexp.QuoteMeta(keyword)
+		kw2sha[keyword] = "isuda_" + fmt.Sprintf("%x", sha1.Sum([]byte(keyword)))
+		keywords = append(keywords, keyword)
+		keywords = append(keywords, kw2sha[keyword])
 	}
 	rows.Close()
 
-	re := regexp.MustCompile("(" + strings.Join(keywords, "|") + ")")
+	//re := regexp.MustCompile("(" + strings.Join(keywords, "|") + ")")
+	replacer := strings.NewReplacer(keywords...)
 
-	kw2sha := make(map[string]string)
-	content = re.ReplaceAllStringFunc(content, func(kw string) string {
-		kw2sha[kw] = "isuda_" + fmt.Sprintf("%x", sha1.Sum([]byte(kw)))
-		return kw2sha[kw]
-	})
-	fmt.Println("CONTENT1: " + content)
+	content = replacer.Replace(content)
+	//content = re.ReplaceAllStringFunc(content, func(kw string) string {
+	//		return kw2sha[kw]
+	//})
+
 	content = html.EscapeString(content)
-	fmt.Println("CONTENT2: " + content)
 	for kw, hash := range kw2sha {
 		u, err := r.URL.Parse(baseUrl.String() + "/keyword/" + pathURIEscape(kw))
 		panicIf(err)
 		link := fmt.Sprintf("<a href=\"%s\">%s</a>", u, html.EscapeString(kw))
 		content = strings.Replace(content, hash, link, -1)
 	}
-	fmt.Println("CONTENT3: " + content)
 
 	return strings.Replace(content, "\n", "<br />\n", -1)
 }
