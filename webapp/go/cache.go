1402b6b4b16442958da930198d8022829c115b86
pokotsun <gorigolila@gmail.com>
2019-07-04 18:13:46 +0900

    return 2 value on getReplacerFromCache func

--- webapp/go/cache.go
+++ webapp/go/cache.go
@@ -34,7 +34,7 @@ func GetStarNumFromCache(keyword string) (int, bool) {
 	return num, found
 }
 
-func GetHtmlifyReplacerFromCache() *strings.Replacer {
+func GetHtmlifyReplacerFromCache() (*strings.Replacer, bool) {
 	key := HTMLIFY_REPLACER
 	data_i, found := getDataFromCache(key)
 	if found {
