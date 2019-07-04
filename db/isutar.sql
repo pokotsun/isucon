01a77c7c26cc63602538d86c94375bf33afe04e4
pokotsun <gorigolila@gmail.com>
2019-07-04 18:06:26 +0900

    append keyword index to star

--- db/isutar.sql
+++ db/isutar.sql
@@ -2,5 +2,6 @@ CREATE TABLE star (
     id BIGINT UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
     keyword VARCHAR(191) NOT NULL,
     user_name VARCHAR(191) NOT NULL,
-    created_at DATETIME
+    created_at DATETIME,
+    INDEX keyword_idx(keyword)
 ) Engine=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
