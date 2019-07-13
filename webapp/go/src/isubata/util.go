bbaccb3c83df8c75d8bf0dc016a32467805b61c6
naoto67 <uver.com67@gmail.com>
2019-07-13 10:28:40 +0900

    fix query

--- webapp/go/src/isubata/util.go
+++ webapp/go/src/isubata/util.go
@@ -16,7 +16,33 @@ func tRange(a, b int64) []int64 {
 
 func jsonifyMessageWith(chanID, lastID int64) ([]map[string]interface{}, error) {
 	response := make([]map[string]interface{}, 0, 100)
-	rows, err := db.Queryx("SELECT m.id, m.content, m.created_at, u.name, u.display_name, u.avatar_icon FROM message m INNER JOIN user u on u.id = m.user_id WHERE id > ? AND channel_id = ? ORDER BY id DESC LIMIT 100", lastID, chanID)
+	rows, err := db.Queryx("SELECT m.id, m.content, m.created_at, u.name, u.display_name, u.avatar_icon FROM message m INNER JOIN user u on u.id = m.user_id WHERE m.id > ? AND m.channel_id = ? ORDER BY m.id DESC LIMIT 100", lastID, chanID)
+	if err != nil {
+		return response, err
+	}
+	for rows.Next() {
+		var m Message
+		var u User
+		err = rows.Scan(&m.ID, &m.Content, &m.CreatedAt, &u.Name, &u.DisplayName, &u.AvatarIcon)
+		if err != nil {
+			return response, err
+		}
+		r := make(map[string]interface{})
+		r["id"] = m.ID
+		r["user"] = u
+		r["date"] = m.CreatedAt.Format("2006/01/02 15:04:05")
+		r["content"] = m.Content
+		response = append(response, r)
+	}
+	return response, nil
+}
+
+func jsonifyMessageWithChannel(chID, limit, offset int64) ([]map[string]interface{}, error) {
+	response := make([]map[string]interface{}, 0, 100)
+	rows, err := db.Queryx("SELECT m.id, m.content, m.created_at, u.name, u.display_name, u.avatar_icon FROM message m INNER JOIN user u on u.id = m.user_id WHERE m.channel_id = ? ORDER BY m.id DESC LIMIT ? OFFSET ?", chID, limit, offset)
+	if err != nil {
+		return response, err
+	}
 	for rows.Next() {
 		var m Message
 		var u User
