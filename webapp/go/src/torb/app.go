7e23c48d31bb74750f26b57769a2d863a33286dd
naoto67 <uver.com67@gmail.com>
2019-08-03 10:45:55 +0900

    divide user

--- webapp/go/src/torb/app.go
+++ webapp/go/src/torb/app.go
@@ -23,13 +23,6 @@ import (
 	"github.com/labstack/echo/middleware"
 )
 
-type User struct {
-	ID        int64  `json:"id,omitempty"`
-	Nickname  string `json:"nickname,omitempty"`
-	LoginName string `json:"login_name,omitempty"`
-	PassHash  string `json:"pass_hash,omitempty"`
-}
-
 type Event struct {
 	ID       int64  `json:"id,omitempty"`
 	Title    string `json:"title,omitempty"`
@@ -84,37 +77,6 @@ type Administrator struct {
 	PassHash  string `json:"pass_hash,omitempty"`
 }
 
-func sessUserID(c echo.Context) int64 {
-	sess, _ := session.Get("session", c)
-	var userID int64
-	if x, ok := sess.Values["user_id"]; ok {
-		userID, _ = x.(int64)
-	}
-	return userID
-}
-
-func sessSetUserID(c echo.Context, id int64) {
-	sess, _ := session.Get("session", c)
-	sess.Options = &sessions.Options{
-		Path:     "/",
-		MaxAge:   3600,
-		HttpOnly: true,
-	}
-	sess.Values["user_id"] = id
-	sess.Save(c.Request(), c.Response())
-}
-
-func sessDeleteUserID(c echo.Context) {
-	sess, _ := session.Get("session", c)
-	sess.Options = &sessions.Options{
-		Path:     "/",
-		MaxAge:   3600,
-		HttpOnly: true,
-	}
-	delete(sess.Values, "user_id")
-	sess.Save(c.Request(), c.Response())
-}
-
 func sessAdministratorID(c echo.Context) int64 {
 	sess, _ := session.Get("session", c)
 	var administratorID int64
@@ -162,16 +124,6 @@ func adminLoginRequired(next echo.HandlerFunc) echo.HandlerFunc {
 		}
 		return next(c)
 	}
-}
-
-func getLoginUser(c echo.Context) (*User, error) {
-	userID := sessUserID(c)
-	if userID == 0 {
-		return nil, errors.New("not logged in")
-	}
-	var user User
-	err := db.QueryRow("SELECT id, nickname FROM users WHERE id = ?", userID).Scan(&user.ID, &user.Nickname)
-	return &user, err
 }
 
 func getLoginAdministrator(c echo.Context) (*Administrator, error) {
@@ -273,15 +225,6 @@ func sanitizeEvent(e *Event) *Event {
 	sanitized.PublicFg = false
 	sanitized.ClosedFg = false
 	return &sanitized
-}
-
-func fillinUser(next echo.HandlerFunc) echo.HandlerFunc {
-	return func(c echo.Context) error {
-		if user, err := getLoginUser(c); err == nil {
-			c.Set("user", user)
-		}
-		return next(c)
-	}
 }
 
 func fillinAdministrator(next echo.HandlerFunc) echo.HandlerFunc {
