e4cf9dee4faf11b5f2ba7101e48864a5d7396925
pokotsun <gorigolila@gmail.com>
2019-08-03 02:51:27 +0000

    fixed syntax error

--- db/schema.sql
+++ db/schema.sql
@@ -32,8 +32,7 @@ CREATE TABLE IF NOT EXISTS reservations (
     KEY event_id_and_sheet_id_idx (event_id, sheet_id),
     KEY user_id_idx (user_id),
     KEY event_id_idx (event_id),
-    KEY reserved_at_idx (reserved_at_idx),
-
+    KEY reserved_at_idx (reserved_at)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS administrators (
