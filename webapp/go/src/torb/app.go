e489bffabee85dd09e88c8e101dcbf7184eea0e5
naoto67 <uver.com67@gmail.com>
2019-08-03 10:49:42 +0900

    divide sheet

--- webapp/go/src/torb/app.go
+++ webapp/go/src/torb/app.go
@@ -23,25 +23,6 @@ import (
 	"github.com/labstack/echo/middleware"
 )
 
-type Sheets struct {
-	Total   int      `json:"total"`
-	Remains int      `json:"remains"`
-	Detail  []*Sheet `json:"detail,omitempty"`
-	Price   int64    `json:"price"`
-}
-
-type Sheet struct {
-	ID    int64  `json:"-"`
-	Rank  string `json:"-"`
-	Num   int64  `json:"num"`
-	Price int64  `json:"-"`
-
-	Mine           bool       `json:"mine,omitempty"`
-	Reserved       bool       `json:"reserved,omitempty"`
-	ReservedAt     *time.Time `json:"-"`
-	ReservedAtUnix int64      `json:"reserved_at,omitempty"`
-}
-
 type Reservation struct {
 	ID         int64      `json:"id"`
 	EventID    int64      `json:"-"`
@@ -131,12 +112,6 @@ func fillinAdministrator(next echo.HandlerFunc) echo.HandlerFunc {
 		}
 		return next(c)
 	}
-}
-
-func validateRank(rank string) bool {
-	var count int
-	db.QueryRow("SELECT COUNT(*) FROM sheets WHERE `rank` = ?", rank).Scan(&count)
-	return count > 0
 }
 
 type Renderer struct {
---
+++ webapp/go/src/torb/sheet.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"time"
+)
+
+type Sheets struct {
+	Total   int      `json:"total"`
+	Remains int      `json:"remains"`
+	Detail  []*Sheet `json:"detail,omitempty"`
+	Price   int64    `json:"price"`
+}
+
+type Sheet struct {
+	ID    int64  `json:"-"`
+	Rank  string `json:"-"`
+	Num   int64  `json:"num"`
+	Price int64  `json:"-"`
+
+	Mine           bool       `json:"mine,omitempty"`
+	Reserved       bool       `json:"reserved,omitempty"`
+	ReservedAt     *time.Time `json:"-"`
+	ReservedAtUnix int64      `json:"reserved_at,omitempty"`
+}
+
+func validateRank(rank string) bool {
+	var count int
+	db.QueryRow("SELECT COUNT(*) FROM sheets WHERE `rank` = ?", rank).Scan(&count)
+	return count > 0
+}
