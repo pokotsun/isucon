81ab1638c7cfcaed92bf5efc8a629c3b2f7ab515
naoto67 <uver.com67@gmail.com>
2019-08-03 10:54:53 +0900

    divide reservation

--- webapp/go/src/torb/app.go
+++ webapp/go/src/torb/app.go
@@ -22,22 +22,6 @@ import (
 	"github.com/labstack/echo/middleware"
 )
 
-type Reservation struct {
-	ID         int64      `json:"id"`
-	EventID    int64      `json:"-"`
-	SheetID    int64      `json:"-"`
-	UserID     int64      `json:"-"`
-	ReservedAt *time.Time `json:"-"`
-	CanceledAt *time.Time `json:"-"`
-
-	Event          *Event `json:"event,omitempty"`
-	SheetRank      string `json:"sheet_rank,omitempty"`
-	SheetNum       int64  `json:"sheet_num,omitempty"`
-	Price          int64  `json:"price,omitempty"`
-	ReservedAtUnix int64  `json:"reserved_at,omitempty"`
-	CanceledAtUnix int64  `json:"canceled_at,omitempty"`
-}
-
 func loginRequired(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		if _, err := getLoginUser(c); err != nil {
