dcd013b0ef8c3c1e01aad08a3d534b78d10d3e1a
naoto67 <uver.com67@gmail.com>
2019-08-17 15:22:47 +0900

    remove 5001

--- go/src/isuda/isuda.go
+++ go/src/isuda/isuda.go
@@ -75,10 +75,6 @@ func initializeHandler(w http.ResponseWriter, r *http.Request) {
 	FLUSH_ALL()
 	initReplacerToCache(r)
 
-	resp, err := http.Get(fmt.Sprintf("%s/initialize", isutarEndpoint))
-	panicIf(err)
-	defer resp.Body.Close()
-
 	re.JSON(w, http.StatusOK, map[string]string{"result": "ok"})
 }
 
